b2cfe0d2e072e4b8a0984dd48eadb020359a2b1e
z1298752778 <1298752778@qq.com>
2025-02-23 10:47:03 +0800

    edit..

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+for _ in range(10):
+    print("你好")
+    print("你好2")
+# 输出20次“你好”
+for _ in range(20):
+    print("你好")
+# 输出
+test1 = 1
+test2 = "你好"
+test3 = 1.0
+print(type(test1))
+print(type(test2))
+print(type(test3))
